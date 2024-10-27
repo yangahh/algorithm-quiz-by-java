2d776f5d4cd9862f73fb0ceb67628d1ea594b890
yangahh <toaur6802@naver.com>
2024-10-27 18:04:59 +0900

    fix: unchecked warning about Generic array

--- dfsbfs/b18352/Solution.java
+++ dfsbfs/b18352/Solution.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -10,7 +12,7 @@ public class Solution {
     static int m;
     static int k;
     static int x;
-    static LinkedList<Integer>[] graph;
+    static List<List<Integer>> graph;
 
     public static void main(String[] args) throws IOException {
         setInputData();
@@ -22,7 +24,7 @@ public class Solution {
 
         while (!q.isEmpty()) {
             int node = q.poll();
-            for (int next_node : graph[node]) {
+            for (int next_node : graph.get(node)) {
                 if (visited[next_node] == false) {
                     visited[next_node] = true;
                     distance[next_node] = distance[node] + 1;
@@ -43,16 +45,16 @@ public class Solution {
         k = Integer.parseInt(st.nextToken());
         x = Integer.parseInt(st.nextToken());
 
-        graph = new LinkedList[n + 1];  // 노드 번호가 1부터 시작할 때
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new LinkedList<Integer>();
+        graph = new ArrayList<>(n + 1); // 노드 번호가 1부터 시작할 때
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
             int node = Integer.parseInt(st.nextToken());
             int adjacentNode = Integer.parseInt(st.nextToken());
-            graph[node].add(adjacentNode);
+            graph.get(node).add(adjacentNode);
         }
 
         br.close();
